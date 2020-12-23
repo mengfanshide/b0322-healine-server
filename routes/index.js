c96d2ff2015f68619451e382ae54d971dbe55ebb
MonkeyKing <monkeyking@sky.com>
2020-12-23 10:52:02 +0800

    验证-响应拦截

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  /* 
+router.get("/", function (req, res, next) {
+	// res.render('index', { title: 'Express' });
+	/* 
     对请求放返回的内容: {code: xxx, data: xxx}
     code 和前台约定好的状态码, 
     data 请求的数据
   */
 
-  // console.log(req.headers.authorization);
-  const Bearer = req.headers.authorization;
-  const token = Bearer.substr(7);
-  //jwt.verify 会解析错误 包括过期
-  //jwt.decode 直接解析token
-  jwt.verify(token, 'Buka-B0322', (error, decode)=>{
-    console.log(error, 'error');
-    console.log(decode);
-  });
-  res.json({
-    code: 200,
-    data: {
-      title: "继续集火刘佳峰",
-      body: "来啊, 打我啊!!!"
-    }
-  });
+	// console.log(req.headers.authorization);
+	const Bearer = req.headers.authorization;
+	const token = Bearer.substr(7);
+
+	jwt.verify(token, "Buka-B0322", (error, decode) => {
+		if (error) {
+			return res.status(401).json(error);
+		}
+		return res.json({
+			code: 200,
+			data: {
+				title: "继续集火佳峰刘刘酱",
+				body: "来啊, 打我啊!!!",
+			},
+		});
+	});
+
+	//jwt.verify 会解析错误 包括过期
+	//jwt.decode 直接解析token
+	//jwt 所有有效信息都在客户端
+	// jwt.verify(token, 'Buka-B0322', (error, decode)=>{
+	//   // console.log(error, 'error');
+	//   // console.log(decode);
+	//   if(error){
+	//     return res.status(401).json({
+	//       error
+	//     });
+	//   }
+	// });
+	// res.status(401).json({
+	//   code: 200,
+	//   data: {
+	//     title: "继续集火刘佳峰",
+	//     body: "来啊, 打我啊!!!"
+	//   }
+	// });
 });
 
 module.exports = router;
